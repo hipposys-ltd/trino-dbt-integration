592c739615491807010b08b2aa12821bdeff30d1
Egor Tarasenko <egor@ponder.co>
2025-01-06 17:49:54 +0200

    FEAT: Change apps order

--- all_the_apps.sh
+++ all_the_apps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Open URLs in Google Chrome
-open -a "Google Chrome" "http://localhost:8888" "http://localhost:8089" "http://localhost:8080" "http://localhost:80" 
+open -a "Google Chrome" "http://localhost:8089" "http://localhost:8888" "http://localhost:80" "http://localhost:8080" 
